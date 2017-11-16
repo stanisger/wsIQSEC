70924553da5371be400067cfcdabffb230fd097c
pablo maya <mayajahir8@gmail.com>
2017-11-16 00:47:42 -0600

    arreglar detalle en boton curps

--- js/app.js
+++ js/app.js
@@ -12,17 +12,17 @@ var nextId = 1;
 var  inputContent = document.getElementById('inputContent');
 
 function addLabel() {
-    var input = document.createElement("input");
-    var btn = document.getElementById('btn_curps');
-    input.setAttribute("type", "text") ;
-    input.setAttribute("placeholder", "Ingrese su CURP*");
-
-    input.id = "curp" +'_' + nextId;
-    input.name = "curp" +'_' + nextId;
-
-    btn.onclick = addLabel;
-    inputContent.appendChild(input);
-    nextId += 1;
+	if(nextId<=5){
+		var input = document.createElement("input");
+	    var btn = document.getElementById('btn_curps');
+	    input.setAttribute("type", "text") ;
+	    input.setAttribute("placeholder", "Ingrese su CURP*");
+	    input.id = "curp" +'_' + nextId;
+	    input.name = "curp" +'_' + nextId;	
+	    inputContent.appendChild(input);
+	    btn.onclick = addLabel;    
+    	nextId += 1;
+	}    
 }
 
 addLabel();
